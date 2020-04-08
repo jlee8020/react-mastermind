3424e16700dd445aa390ea6ee1be1a4e7c629bb1
Jan Lee <janlee@Jans-MacBook-Pro.local>
2020-04-08 13:44:06 -0500

    deploy

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,14 @@
-const User = require('../models/user');
+// const User = require('../models/user');
 
-module.exports = {
-  signup
-};
+// module.exports = {
+//   signup
+// };
 
-async function signup(req, res) {
-  try {
-    const user = await User.create(req.body);
-    res.json(user);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-} 
+// async function signup(req, res) {
+//   try {
+//     const user = await User.create(req.body);
+//     res.json(user);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// } 
--- server.js
+++ server.js
@@ -3,36 +3,26 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
-
-require('dotenv').config();
-// require('./config/dabase');
-
-
 const app = express();
+
+// require('dotenv').config();
+// require('./config/database');
 
 app.use(logger('dev'));
 app.use(express.json());
-
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.json());
 
 // Put API routes here, before the "catch all" route
+// app.use('/api', require('./routes/api/users'));
 
-// The following "catch all" route (note the *)is necessary
-// for a SPA's client-side routing to properly work 
+// "catch all" route
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.use('/api', require('./routes/api/users'));
-
-
-// Configure to use port 3001 instead of 3000 during
-// development to avoid collision with React's dev server
 const port = process.env.PORT || 3001;
 
-
 app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
+  console.log(`Express app running on port ${port}`);
 });
