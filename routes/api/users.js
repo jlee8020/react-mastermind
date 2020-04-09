c79a68451337e9f743eb65e02a1a9effbdb8b3b7
Jan Lee <janlee@Jans-MacBook-Pro.local>
2020-04-08 20:37:02 -0500

    troubleshooting

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,9 @@
-// const express = require('express');
-// const router = express.Router();
-// const usersCtrl = require('../../controllers/users');
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/users');
 
 
-// router.post('/signup', usersCtrl.signup);
+router.post('/signup', usersCtrl.signup);
 
 
-// module.exports = router;
+module.exports = router;
